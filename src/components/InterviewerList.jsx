8cf2048f284cf39456e879787645cb8d82ee2830
salimM-code <mustafasalim000@gmail.com>
2021-11-02 14:38:52 -0400

    added a prop type check

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 const InterviewerList = (props) => {
   const {interviewers, onChange, value} = props;
@@ -23,5 +24,9 @@ const InterviewerList = (props) => {
   </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers:PropTypes.array.isRequired
+};
 
 export default InterviewerList
